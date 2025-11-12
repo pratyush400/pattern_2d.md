9bcdd2f685a9c993e82074e616b9f1719e2253c0
EmEdenbrooke <153880994+EmEdenbrooke@users.noreply.github.com>
2025-11-12 11:34:38 -0800

    Add files via upload

--- brute_force_match.py
+++ brute_force_match.py
@@ -1,16 +1,32 @@
 
-def match(P, T):
-    for i in range(0, len(T)-len(P)+1):
-        for j in range(0, len(T)-len(P)+1):
-            match = True
-            for x in range(0, len(P)):
-                for y in range(0, len(P)):
-                    if P[x][y] != T[i+x][j+y]:
-                        match = False   
-                        break
-            if match: 
-                return (i,j)             
-            
-                            
+def match(pattern, text):
+    pattern_row = len(pattern)
+    pattern_column = len(pattern[0])
+    text_row = len(text)
+    text_column = len(text[0])
 
-            
+    if pattern_row > text_row or pattern_column > text_column:
+        return False  # pattern can not be larger than text
+
+    ## checking pattern in a row...
+
+    for i in range(text_row - pattern_row + 1):  ##checks max each row ends if remaining text is too short for pattern
+        for j in range(text_column - pattern_column + 1):  ##checks max each column
+            match_found = True
+            ## checks if there is a match starting and i row and j col
+            for r in range(pattern_row): # checks the text's rows for pattern
+                for c in range(pattern_column):
+                    if text[i + r][j + c] != pattern[r][c]:
+                        match_found = False
+                        break
+                if not match_found:
+                    break
+            if match_found:
+                 return i,j
+    return None
+
+
+
+
+
+
