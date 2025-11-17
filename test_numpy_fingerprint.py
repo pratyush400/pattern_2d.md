f15ed301cec3ead99a77480209ca537da1a7edfd
Pratyush <lc23-0429@lclark.edu>
2025-11-17 12:21:25 -0800

    Refactor tests for numpy fingerprint matching
    Updated test cases to include additional data and assertions.

--- test_numpy_fingerprint.py
+++ test_numpy_fingerprint.py
@@ -3,14 +3,15 @@ from numpy_fingerprint_match import match
 
 P1 = [
     [1,2,3],
-    [2,3,1]
-]
-T1 = [
-    [5,6,7],
-    [5,32,1],
-    [1,2,3],
     [2,3,1],
     [4,2,1]
+]
+T1 = [
+    [5,6,7,0,0],
+    [5,32,1,0,0],
+    [1,2,3,0,0],
+    [2,3,1,0,0],
+    [4,2,1,0,0]
 ]
 P2 = [
     [1,0],
@@ -22,11 +23,10 @@ T2 = [
     [8,0,1,9],
     [6,7,2,3]
 ]
-
-def test_np_match_1():
-    print(match(P1,T1))
-    assert match(P1,T1) == (1,1)
-
-def test_np_match_2():
+def test_if_match():
     print(match(P2,T2))
+    print(match(P1,T1))
     assert match(P2,T2) == (1,1)
+
+def test_2():
+    assert match(P1,T1) == (2,0)
